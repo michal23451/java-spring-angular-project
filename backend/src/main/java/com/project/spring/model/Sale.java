e7afee9a27a5320acee9205e9bba79d32b7e04a9
Michał Konowalski <konowalski.michal@gmail.com>
2022-10-09 09:55:09 +0200

    initial commit

---
+++ backend/src/main/java/com/project/spring/model/Sale.java
@@ -0,0 +1,19 @@
+package com.project.spring.model;
+
+/*
+* product name
+* sale date and time
+* quantity
+* unit price
+* contract number - nie wysyłamy tego w DTO
+*
+* Constraints"
+* - nie można dodać więcej niż jednej sprzedaży produktu o tej samej nazwie tego samego dnia i tego samego sprzedawcy
+*    - zamiast tego
+*       - quantity sumujemy
+*       - obliczamy średnią cenę za jednostkę
+*  -
+*/
+
+public class Sale {
+}
